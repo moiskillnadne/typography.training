2413462ca71a9be22ce23adb1ecf6525e7f4bb1d
Victor Ryabkov <victor.ryabkov.business@gmail.com>
2021-10-03 02:51:30 +0300

    feature: finished main page mobile adaptive

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+const root = document.querySelector('.root');
 const loginButton = document.querySelector('#login_button');
+const loginPopup = document.querySelector('#login_popup');
+
+const loginPopupStatusHidden = 'none'
+const loginPopupStatusShow = 'block'
 
 loginButton.addEventListener('click', (e) => {
-    console.log(e)
+    const buttonRect = loginButton.getBoundingClientRect()
+    e.stopPropagation()
+
+    if(loginPopup.style.display === loginPopupStatusHidden || loginPopup.style.display === '') {
+        loginPopup.style.display = loginPopupStatusShow;
+        loginPopup.style.left = `${buttonRect.x - 180}px`
+    } else {
+        loginPopup.style.display = loginPopupStatusHidden;
+    }
+})
+
+root.addEventListener('click', () => {
+    if(loginPopup.style.display === loginPopupStatusShow) {
+        loginPopup.style.display = loginPopupStatusHidden;
+    }
 })
