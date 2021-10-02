5641030c14c1cb21561d61bb7ead07b530e320b1
Victor Ryabkov <victor.ryabkov.business@gmail.com>
2021-10-02 10:19:57 +0300

    fix: links and hover effects

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
+const loginButton = document.querySelector('#login_button');
 
-
-console.log('some 1')
-setTimeout(() => Promise.resolve(5).then((res) => console.log('some 2')))
-setTimeout(() => console.log('some 3'))
-Promise.resolve(5).then((res) => console.log('some 4'))
-console.log('some 5')
+loginButton.addEventListener('click', (e) => {
+    console.log(e)
+})
