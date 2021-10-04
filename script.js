355e415d8f98adac0359dcff1fd291ada7250279
Victor Ryabkov <victor.ryabkov.business@gmail.com>
2021-10-04 19:47:06 +0300

    feature: added fixes to main desktop page and added mobile burger menu

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 const root = document.querySelector('.root');
+const windowWidth = window.innerWidth
 const loginButton = document.querySelector('#login_button');
 const loginPopup = document.querySelector('#login_popup');
+const payButtonRIAO = document.querySelector('#rule_inside_and_outside_pay_button');
+
+const burgerMenuButton = document.querySelector('.menu_burger_mid_line');
+const menuLayerMobile = document.querySelector('#menu_layer_mobile');
 
 const loginPopupStatusHidden = 'none'
 const loginPopupStatusShow = 'block'
 
-loginButton.addEventListener('click', (e) => {
-    const buttonRect = loginButton.getBoundingClientRect()
-    e.stopPropagation()
+const isMobileSize = windowWidth < 576
 
-    if(loginPopup.style.display === loginPopupStatusHidden || loginPopup.style.display === '') {
-        loginPopup.style.display = loginPopupStatusShow;
-        loginPopup.style.left = `${buttonRect.x - 180}px`
-    } else {
-        loginPopup.style.display = loginPopupStatusHidden;
-    }
+payButtonRIAO.addEventListener('click', (e) => {
+    window.location.href = './pages/inexternal/0.html'
 })
 
-root.addEventListener('click', () => {
-    if(loginPopup.style.display === loginPopupStatusShow) {
-        loginPopup.style.display = loginPopupStatusHidden;
-    }
+if(!isMobileSize) {
+    loginButton.addEventListener('click', (e) => {
+        const buttonRect = loginButton.getBoundingClientRect()
+        e.stopPropagation()
+    
+        if(loginPopup.style.display === loginPopupStatusHidden || loginPopup.style.display === '') {
+            loginPopup.style.display = loginPopupStatusShow;
+            loginPopup.style.left = `${buttonRect.x - 180}px`
+        } else {
+            loginPopup.style.display = loginPopupStatusHidden;
+        }
+    })
+    
+    root.addEventListener('click', () => {
+        if(loginPopup.style.display === loginPopupStatusShow) {
+            loginPopup.style.display = loginPopupStatusHidden;
+        }
+    })
+}
+
+burgerMenuButton.addEventListener('click', (e) => {
+    console.log(e)
+
+    burgerMenuButton.classList.toggle('menu_burger_mid_line_active')
+    menuLayerMobile.classList.toggle('menu_layer_mobile_active')
 })
