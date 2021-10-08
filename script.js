6f4424334ae2019bd8961a246f694bbfa98e81be
Victor Ryabkov <victor.ryabkov.business@gmail.com>
2021-10-08 16:39:17 +0300

    feature: finished auth

--- script.js
+++ script.js
@@ -1,62 +1,178 @@
-const root = document.querySelector('.root');
-const windowWidth = window.innerWidth
-const loginButton = document.querySelector('#login_button');
-const loginPopup = document.querySelector('#login_popup');
-const payButtonRIAO = document.querySelector('#rule_inside_and_outside_pay_button');
-const payButtonAnchorObjects = document.querySelector('#anchor_objects')
+const backend = 'https://typography.training/api'
+const devStand = 'http://localhost:5555/api'
 
-const burgerMenuButton = document.querySelector('.menu_burger_mid_line');
-const menuLayerMobile = document.querySelector('#menu_layer_mobile');
+window.onload = async () => {
+    const root = document.querySelector('.root');
+    const windowWidth = window.innerWidth
 
-const loginPopupStatusHidden = 'none'
-const loginPopupStatusShow = 'block'
+    const loginButton = document.querySelector('#login_button');
+    const loginPopup = document.querySelector('#login_popup');
+    const accountButton = document.querySelector('#account_button');
+    const accountPopup = document.querySelector('#account_popup');
+    const logoutButton = document.querySelector('#logout_button');
 
-const loginLayerCloseButton = document.querySelector('.login_layer_close_button')
-const loginLayerMobile = document.querySelector('#login_layer_mobile')
+    const payButtonRIAO = document.querySelector('#rule_inside_and_outside_pay_button');
+    const payButtonAnchorObjects = document.querySelector('#anchor_objects')
 
-const isMobileSize = windowWidth < 576
+    const burgerMenuButton = document.querySelector('.menu_burger_mid_line');
+    const menuLayerMobile = document.querySelector('#menu_layer_mobile');
 
-payButtonRIAO.addEventListener('click', (e) => {
-    window.location.href = './pages/inexternal/0.html'
-})
+    const loginPopupStatusHidden = 'none'
+    const loginPopupStatusShow = 'block'
 
-payButtonAnchorObjects.addEventListener('click', () => {
-    window.location.href = './pages/anchor/1.html'
-})
+    const loginLayerCloseButton = document.querySelector('.login_layer_close_button')
+    const loginLayerMobile = document.querySelector('#login_layer_mobile')
 
-if(!isMobileSize) {
-    loginButton.addEventListener('click', (e) => {
-        const buttonRect = loginButton.getBoundingClientRect()
-        e.stopPropagation()
-    
-        if(loginPopup.style.display === loginPopupStatusHidden || loginPopup.style.display === '') {
-            loginPopup.style.display = loginPopupStatusShow;
-            loginPopup.style.left = `${buttonRect.x - 180}px`
+    const loginInput = document.querySelector('#login_email_input')
+    const sendEmailButton = document.querySelector('#send_email_button')
+    const sendEmailButtonIcon = document.querySelector('#send_email_button_icon')
+
+    const isMobileSize = windowWidth < 576
+
+    payButtonRIAO.addEventListener('click', (e) => {
+        window.location.href = './pages/inexternal/1.html'
+    })
+
+    payButtonAnchorObjects.addEventListener('click', () => {
+        window.location.href = './pages/anchor/1.html'
+    })
+
+    if(localStorage.getItem('email')) {
+        loginButton.style.display = 'none'
+        accountButton.style.display = 'block'
+    } else {
+        loginButton.style.display = 'block'
+        accountButton.style.display = 'none'
+    }
+
+    if(!isMobileSize) {
+        if(loginButton) {
+            loginButton.addEventListener('click', async (e) => {
+                const buttonRect = loginButton.getBoundingClientRect()
+                e.stopPropagation()
+        
+                if(loginPopup.style.display === loginPopupStatusHidden || loginPopup.style.display === '') {
+                    loginPopup.style.display = loginPopupStatusShow;
+                    loginPopup.style.left = `${buttonRect.x - 155}px`
+                } else {
+                    loginPopup.style.display = loginPopupStatusHidden;
+                }
+            })
+            
+            root.addEventListener('click', () => {
+                if(loginPopup.style.display === loginPopupStatusShow) {
+                    loginPopup.style.display = loginPopupStatusHidden;
+                }
+            })
+        }
+
+
+        if(accountButton) {
+            accountButton.addEventListener('click', (e) => {
+                const buttonRect = accountButton.getBoundingClientRect()
+                e.stopPropagation()
+        
+                if(accountPopup.style.display === loginPopupStatusHidden || accountPopup.style.display === '') {
+                    accountPopup.style.display = loginPopupStatusShow;
+                    accountPopup.style.left = `${buttonRect.x - 15}px`
+                } else {
+                    accountPopup.style.display = loginPopupStatusHidden;
+                }
+            })
+        }
+    } else {
+        loginButton.addEventListener('click', () => {
+            loginLayerMobile.classList.add('login_layer_mobile_active')
+        })
+    }
+
+    burgerMenuButton.addEventListener('click', (e) => {
+        console.log(e)
+
+        burgerMenuButton.classList.toggle('menu_burger_mid_line_active')
+        menuLayerMobile.classList.toggle('menu_layer_mobile_active')
+    })
+
+    loginLayerCloseButton.addEventListener('click', () => {
+        loginLayerMobile.classList.remove('login_layer_mobile_active')
+        console.log('close login')
+
+    })
+
+    loginInput.addEventListener('input', (e) => {
+        const email = e.target.value
+
+        if(validateEmail(email)) {
+            sendEmailButton.disabled = false
+            sendEmailButton.style.backgroundColor = '#000000';
         } else {
-            loginPopup.style.display = loginPopupStatusHidden;
+            sendEmailButton.disabled = true
         }
     })
-    
-    root.addEventListener('click', () => {
-        if(loginPopup.style.display === loginPopupStatusShow) {
-            loginPopup.style.display = loginPopupStatusHidden;
+
+    logoutButton.addEventListener('click', () => {
+        localStorage.removeItem('email')
+
+        accountPopup.style.display = loginPopupStatusHidden;
+        loginButton.style.display = 'block'
+        accountButton.style.display = 'none'
+    })
+
+
+    sendEmailButton.addEventListener('click', async (e) => {
+        sendEmailButtonIcon.src = './media/icons/loading.svg'
+        sendEmailButtonIcon.classList.toggle('active_loading')
+
+        const emailValue = loginInput.value
+
+        if(validateEmail(emailValue)) {
+            const resp = await fetch(`${backend}/sendemailcode`, {
+                method: 'POST',
+                mode: 'cors',
+                body: JSON.stringify({
+                    email: emailValue
+                })
+            })
+            const json = await resp.json()
+            if(resp.ok) {
+                console.log(json)
+                sendEmailButtonIcon.classList.toggle('active_loading')
+                sendEmailButtonIcon.src = './media/icons/check.png'
+            }
         }
     })
-} else {
-    loginButton.addEventListener('click', () => {
-        loginLayerMobile.classList.add('login_layer_mobile_active')
-    })
+
+
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    const token = urlParams.get('token')
+
+    console.log('inside domcontentloaded')
+
+    if(token) {
+        const resp = await fetch(`${backend}/verifyToken`, {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify({
+                token
+            })
+        })
+
+        const json = await resp.json()
+
+        if(resp.ok) {
+            localStorage.setItem('email', json.email)
+            window.location.search = ''
+
+            loginButton.style.display = 'none'
+            accountButton.style.display = 'block'
+        }
+        
+    }
 }
 
-burgerMenuButton.addEventListener('click', (e) => {
-    console.log(e)
 
-    burgerMenuButton.classList.toggle('menu_burger_mid_line_active')
-    menuLayerMobile.classList.toggle('menu_layer_mobile_active')
-})
-
-loginLayerCloseButton.addEventListener('click', () => {
-    loginLayerMobile.classList.remove('login_layer_mobile_active')
-    console.log('close login')
-
-})
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
